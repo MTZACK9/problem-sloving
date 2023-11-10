7841dfc063d62d8bc5e8f36e7bf4caede94a7616
Zakaria MAIT <ziko.mait14@gmail.com>
2023-11-10 20:32:45 +0100

    Compare The Triplets

---
+++ src/warmup/problem_solving/CompareTheTriplets.java
@@ -0,0 +1,33 @@
+package warmup.problem_solving;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CompareTheTriplets {
+    /*
+     * Complete the 'compareTriplets' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts the following parameters:
+     *  1. INTEGER_ARRAY a
+     *  2. INTEGER_ARRAY b
+     */
+    public static List<Integer> compareTriplets(List<Integer> a, List<Integer> b) {
+        int alicePoints = 0;
+        int bobPoints = 0;
+
+        for (int i = 0; i < 3; i++){
+            if(a.get(i) > b.get(i)){
+                alicePoints += 1;
+            } else if (a.get(i) < b.get(i)) {
+                bobPoints += 1;
+            }
+        }
+
+        List<Integer> result = new ArrayList<>();
+        result.add(alicePoints);
+        result.add(bobPoints);
+
+        return result;
+    }
+}
