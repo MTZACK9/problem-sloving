cf8cf30af34d70ecd52f78735a058d8be9c9d226
Zakaria MAIT <ziko.mait14@gmail.com>
2023-11-11 17:21:32 +0100

    Grading Students

--- src/implementation/problem_solving/GradingStudents.java
+++ src/implementation/problem_solving/GradingStudents.java
@@ -14,12 +14,25 @@ public class GradingStudents {
      * The function accepts INTEGER_ARRAY grades as parameter.
      */
 
-    public static List<Integer> gradingStudents(List<Integer> grades){
+    public static List<Integer> gradingStudents(List<Integer> grades) {
 
         List<Integer> finalGrades = new ArrayList<>();
 
+        int nextMultipleFive;
+
+        for (Integer grade : grades) {
+            if (grade < 38) {
+                finalGrades.add(grade);
+            } else {
+                nextMultipleFive = (grade / 5 + 1) * 5;
+                if ((nextMultipleFive - grade) >= 3) {
+                    finalGrades.add(grade);
+                } else {
+                    finalGrades.add(nextMultipleFive);
+                }
+            }
+        }
 
         return finalGrades;
-
     }
 }
