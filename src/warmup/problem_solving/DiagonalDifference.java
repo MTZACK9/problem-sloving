3ac60b4e2119cb3583a1b7694cec2a96567745cd
Zakaria MAIT <ziko.mait14@gmail.com>
2023-11-11 01:29:40 +0100

    Diagonal Difference

---
+++ src/warmup/problem_solving/DiagonalDifference.java
@@ -0,0 +1,24 @@
+package warmup.problem_solving;
+
+import java.util.List;
+
+public class DiagonalDifference {
+
+    /*
+     * Complete the 'diagonalDifference' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts 2D_INTEGER_ARRAY arr as a parameter.
+     */
+
+    public static int diagonalDifference(List<List<Integer>> arr) {
+        int ltr = 0;
+        int rtl = 0;
+        int n = arr.size() - 1;
+        for (int i = 0; i < arr.size(); i++) {
+            ltr += arr.get(i).get(i);
+            rtl += arr.get(i).get(n - i);
+        }
+        return Math.abs(ltr - rtl);
+    }
+}
