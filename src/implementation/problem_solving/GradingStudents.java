ffa0d8825ef11aa21b8e34eddfd0d0cd76a34467
Zakaria MAIT <ziko.mait14@gmail.com>
2023-11-11 15:54:53 +0100

    Grading Students

---
+++ src/implementation/problem_solving/GradingStudents.java
@@ -0,0 +1,25 @@
+package implementation.problem_solving;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GradingStudents {
+
+    // https://www.hackerrank.com/challenges/grading/problem
+
+    /*
+     * Complete the 'gradingStudents' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts INTEGER_ARRAY grades as parameter.
+     */
+
+    public static List<Integer> gradingStudents(List<Integer> grades){
+
+        List<Integer> finalGrades = new ArrayList<>();
+
+
+        return finalGrades;
+
+    }
+}
